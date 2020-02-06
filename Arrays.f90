838dfd96933a38d90966b4d4e67edb8a07a7ecc0
mkhernandez007 <60518605+mkhernandez007@users.noreply.github.com>
2020-02-06 14:33:46 -0500

    Update Arrays.f90

--- Arrays.f90
+++ Arrays.f90
@@ -11,47 +11,47 @@ C/ ./Arrays /
 C/====|================================================================/
       PROGRAM Arrays
       implicit none
-CC
-C Define the two paramters values single and double. These values
+C
+CC Define the two paramters values single and double. These values
 CC never change in the program.
 C
       integer*4, parameter :: single = 4
       integer*4, parameter :: double = 8
       integer*4, parameter :: arrsize = 100
       integer*4, nlats, nlons, status
-CC
-C Define a bunch of fixed length arrays.
+C
+CC Define a bunch of fixed length arrays.
 C
       integer*4, dimension(arrsize) :: intarraysinglea
       integer*4 :: intarraysingleb(-arrsize/2:arrsize/2)
       real(KIND=double), dimension(100,100) :: floatarraydouble
-CC
-C Define one allocatable array.
+C
+CC Define one allocatable array.
 C
       real(KIND=single), ALLOCATABLE, dimension(:,:) :: floatarray
-CC
-C Set the size of the allocatable arrays.
+C
+CC Set the size of the allocatable arrays.
 C
       nlats = 35
       nlons = 70
-CC
-C Allcoate the array.
+C
+CC Allcoate the array.
 C
       ALLOCATE(floatarray(nlats,nlons), STAT=status)
-CC
-C Write to standard output the value of status.
+C
+CC Write to standard output the value of status.
 C
       write(*,*) 'ALLOCATION STATUS = ', status
-CC
-C Write to standard output some of the array values.
+C
+CC Write to standard output some of the array values.
 C
       write(*,*) intarraysinglea(1)
       write(*,*) intarraysinglea(101)
       write(*,*) intarraysingleb(-50)
       write(*,*) floatarraydouble(100,100)
       write(*,*) floatarray(35,70)
-CC
-C Change the array values.
+C
+CC Change the array values.
 C
       intarraysinglea(1) = 100
       intarraysinglea(100) = 201
@@ -60,12 +60,12 @@ C
       floatarray(36,71) = 23.6 ! out of the bounds many
 ! things can happen (BAD)
       floatarray(35,70) = 23.6
-CC
-C Write to standard output the values of status.
+C
+CC Write to standard output the values of status.
 C
       write(*,*) 'New Values'
-CC
-C Write to standard output some of the array values.
+C
+CC Write to standard output some of the array values.
 C
       write(*,*) intarraysinglea(1)
       write(*,*) intarraysinglea(100)
